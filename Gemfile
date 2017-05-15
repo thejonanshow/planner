3165846886ad0e179fd9bc2b648afe993bfb95f3
Jay Bernal <jjbernal@gmail.com>
2017-05-14 22:08:09 -0500

    addes event view

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'haml', '~> 5.0', '>= 5.0.1'
 gem 'annotate'
 gem 'guard-rspec', require: false
+gem 'bootstrap-sass', '~> 3.3.6'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
 
-  describe "GET #index" do
+  describe '#index' do
     it 'shows all users' do
       get :index
       expect(response).to be_success
     end
   end
 
-  describe '#create new item' do
+  describe '#new' do
+    it 'assigns a new device' do
+      get :new
+      expect(response).to redirect_to events_path
+
+    end
+
+  end
+
+  describe '#create' do
     it 'creates all users' do
       expect{
         post :create, event: FactoryGirl.attributes_for(:event)
